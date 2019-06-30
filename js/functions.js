08da5b7d1704dfa7f60aac2e156ca4d7741d1103
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-06-30 16:56:23 +0200

    Init project

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,19 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem' + 'papier' + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+//printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+//printMessage('Zagrałem' + 'papier' + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 
 
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + '!Jeżeli Twój ruch to nożyce, to wygrywasz!');
+// var computerMove, playerMove;
+//computerMove = 'kamień';
+//printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+//playerMove = 'papier';
+//printMessage('Zagrałem' + playerMove + '!Jeżeli Twój ruch to nożyce, to wygrywasz!');
+
+
+
+
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
