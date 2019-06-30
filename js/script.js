316d934f0f879424db4bc3ec1a530e31caa3b67d
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-06-30 13:12:11 +0200

    Add new file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem' + 'papier' + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove + '!Jeżeli Twój ruch to nożyce, to wygrywasz!');
