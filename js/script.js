a4b42276689455dc4637e2fcec3dba096d2fc724
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-07-01 07:17:56 +0200

    Add code to the game

--- js/script.js
+++ js/script.js
@@ -10,9 +10,6 @@
 
 
 
-
-
-
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -27,8 +24,37 @@ if (randomNumber == '2') {
 } else {
   computerMove = 'nieznany ruch';
 }
+printMessage('Mój ruch: ' + computerMove);
 if (randomNumber == '3') {
   computerMove = 'nożyce';
 } else {
   computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to:' + playerInput);
+if (playerInput == '2') {
+  playerInput = 'papier';
+} else {
+  playerInput = 'nieznany ruch';
+}
+printMessage('Twój ruch:' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź' + playerInput);
+if (playerInput equal '3') {
+  playerInput = 'nożyce';
+} else {
+  playerInput = 'nieznany ruch';
 }
